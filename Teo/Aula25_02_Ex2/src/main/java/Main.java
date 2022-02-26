db7189d7f1a803766b3917054ef59ab62d62f83e
jonathan <jonathan.stefan@inatel.br>
2022-02-25 22:39:26 -0300

    Exercicio 2 da aula 24_02

---
+++ Teo/Aula25_02_Ex2/src/main/java/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner numeroDeZumbis = new Scanner(System.in);
+
+
+        System.out.println("Entre com a quantidade de zumbis mortos na antepenultima rodada");
+        int antepenultima = numeroDeZumbis.nextInt();
+        System.out.println("Entre com a quantidade de zumbis mortos na penultima rodada");
+        int penultima = numeroDeZumbis.nextInt();
+        System.out.println("Entre com a quantidade de zumbis mortos na ultima rodada");
+        int ultima = numeroDeZumbis.nextInt();
+
+        int total = antepenultima + penultima + ultima;
+        float media = (float) total / 3;
+
+        System.out.printf("total de mortos foram: %d \n", + total);
+        System.out.printf("A media de mortes foi: %f \n", + media);
+
+    }
+}
