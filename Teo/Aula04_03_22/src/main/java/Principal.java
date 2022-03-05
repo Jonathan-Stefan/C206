acf3c6794f55bb769c6d5ceeb1b507c8648dbe7c
jonathan <jonathan.stefan@inatel.br>
2022-03-04 22:44:18 -0300

    Aula 04_03_22 Usando biblioteca lombok

---
+++ Teo/Aula04_03_22/src/main/java/Principal.java
@@ -0,0 +1,17 @@
+public class Principal {
+
+    public static void main(String[] args) {
+
+        /*Conta c = new Conta(500, 10);//Essa conta usa o construtor com parametros
+
+        System.out.println(c.getSaldo());
+
+        Conta c2 = new Conta(); //Essa conta usa o construtor sem parametros
+        c2.depositar(5);
+        System.out.println(c2.getSaldo());*/
+
+        Pessoa p = new Pessoa();
+        System.out.println(p.getNome());
+
+    }
+}
