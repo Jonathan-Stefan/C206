3024dd4d6b66d822b56060e7f30f08ce4f1a8c9b
jonathan <jonathan.stefan@inatel.br>
2022-03-24 20:09:02 -0300

    Usando o static

---
+++ Teo/Aula24_03_22_Static/src/main/java/Soma.java
@@ -0,0 +1,14 @@
+public class Soma {
+
+    public int x = 10;
+
+     public  static int y = 50;
+
+    public static int Soma(int num1, int num2){
+        return num1+num2;
+    }
+
+    public Soma(){
+        y++;
+    }
+}
