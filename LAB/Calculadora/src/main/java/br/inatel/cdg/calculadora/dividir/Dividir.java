2b858f27f1fb5a230b090d28b4d738723f6a4d32
jonathan <jonathan.stefan@inatel.br>
2022-04-28 20:38:35 -0300

    Calculadora usando o Try / Catch

--- LAB/Calculadora/src/main/java/br/inatel/cdg/calculadora/dividir/Dividir.java
+++ LAB/Calculadora/src/main/java/br/inatel/cdg/calculadora/dividir/Dividir.java
@@ -4,6 +4,9 @@ public class Dividir {
 
     public float dividir(float a, float b) throws java.lang.ArithmeticException
     {
+        if (b==0)
+            throw new ArithmeticException();
+        else
         return a/b;
     }
 
