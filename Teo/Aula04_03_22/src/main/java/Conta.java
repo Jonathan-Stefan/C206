371676b09ec1a7c464ac09d09a59dd4c0cc61db6
jonathan <jonathan.stefan@inatel.br>
2022-05-13 22:58:23 -0300

    Aula 19/05/22, try catch

--- Teo/Aula04_03_22/src/main/java/Conta.java
+++ Teo/Aula04_03_22/src/main/java/Conta.java
@@ -1,22 +1,45 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Conta {
 
      private double saldo, limite;
+    Set<Cliente> clientes = new HashSet<>();
 
      public Conta (double saldo, double limite){ //Esse contrutor recebe parametros
          this.saldo = saldo;
          this.limite = limite;
      }
-
-     public Conta(){
-         //Contrutor que não recebe parametros
+     public void adiconaCliente (Cliente cliente){
+         clientes.add(cliente);
      }
 
-    void sacar (double quantia){
-        saldo -= quantia;
+
+    void sacar (double quantia) throws SaldoInsuficienteException{
+         if (saldo > quantia)
+             saldo -= quantia;
+         else
+             throw new SaldoInsuficienteException("Saldo insuficiente");
+
     }
 
     void depositar (double quantia){
         saldo += quantia;
+    }
+
+    public void mostraInfo() {
+
+        System.out.println("Saldo: "+saldo + " Limite: "+limite);
+
+        for (Cliente cliente: clientes)
+        {
+            try{
+                System.out.println("Nome: "+cliente.getNome()+" CPF: "+cliente.getCpf());
+
+            }catch (NullPointerException e){
+                System.out.println("Não existe cliente nessa posição. ");
+            }
+        }
     }
 
     // Getters e Setter -> Devolve e Seta o valor de saldo
@@ -24,5 +47,11 @@ public class Conta {
         return saldo;
     }
 
+    public Set<Cliente> getClientes() {
+        return clientes;
+    }
 
+    public void setClientes(Set<Cliente> clientes) {
+        this.clientes = clientes;
+    }
 }
